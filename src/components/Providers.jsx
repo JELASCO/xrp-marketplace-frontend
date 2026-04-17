21a8e44f2dbb1d9f10d8c64aa36d72d4180a8507
JELASCO <jelomarketplace@gmail.com>
2026-04-17 06:25:08 +0300

    Delete src directory

--- src/components/Providers.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { useEffect } from 'react';
-import { useAuthStore } from '../lib/store';
-
-export default function Providers({ children }) {
-  const init = useAuthStore(s => s.init);
-  useEffect(() => { init(); }, [init]);
-  return <>{children}</>;
-}
