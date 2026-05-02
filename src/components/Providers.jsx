1ededd6b9aa3a872a758e262b7298811bf58dc57
JELASCO <jelomarketplace@gmail.com>
2026-05-02 08:03:41 +0300

    Update Providers.jsx

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,9 +1,24 @@
 'use client';
 import { useEffect } from 'react';
-import { useAuthStore } from '../lib/store';
+import { useAuthStore, useNotificationsStore } from '../lib/store';
 
 export default function Providers({ children }) {
   const init = useAuthStore(s => s.init);
+  const user = useAuthStore(s => s.user);
+  const loadNotifs = useNotificationsStore(s => s.load);
+  const connectSocket = useNotificationsStore(s => s.connectSocket);
+  const disconnectSocket = useNotificationsStore(s => s.disconnectSocket);
+
   useEffect(() => { init(); }, []);
+
+  useEffect(() => {
+    if (user) {
+      loadNotifs();
+      connectSocket(user.id);
+    } else {
+      disconnectSocket();
+    }
+  }, [user]);
+
   return <>{children}</>;
 }
